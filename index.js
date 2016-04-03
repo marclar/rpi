bc7f1c3a020c784aa7aa64f2ba55b84288b9ba32
Michael Kane <mk+github@mk4p.com>
2016-04-03 14:19:55 -0400

    idk

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ device
 
 device
     .on('message', function(topic, payload) {
-        payload = JSON.parse(payload.toString());
-        log('got payload for topic "' + topic + '"');
+        payload = payload.toString();
+        log('got ' + (typeof payload) + ' payload for topic "' + topic + '"');
         log(payload);
     });
